5950534b9f6a1ef640f4d390b7df21f5d1041f43
niuqiang1 <1228918562@qq.com>
2023-03-31 16:38:11 +0800

    up

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,33 +8,29 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig( /** @type {import('astro').AstroUserConfig} */
-{
-  // experimental: {
-  // 	assets: true
-  // },
-  markdown: {
-    render: [astroRemark, {
-      remarkPlugins: [],
-      rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', {
-        behavior: 'wrap'
-      }]],
+  {
+    // experimental: {
+    // 	assets: true
+    // },
+    markdown: {
+      render: [astroRemark, {
+        remarkPlugins: [],
+        rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', {
+          behavior: 'wrap'
+        }]],
+        shikiConfig: {
+          theme: 'poimandres',
+          langs: [],
+          wrap: false
+        }
+      }]
+    },
+    integrations: [mdx({
+      syntaxHighlight: 'shiki',
       shikiConfig: {
-        theme: 'poimandres',
-        langs: [],
-        wrap: false
-      }
-    }]
-  },
-  integrations: [mdx({
-    syntaxHighlight: 'shiki',
-    shikiConfig: {
-      theme: 'poimandres'
-    },
-    gfm: false
-  }), react(),
-  partytown({
-    config: {
-      forward: ["dataLayer.push"],
-    },
-  }),]
-});
+        theme: 'poimandres'
+      },
+      gfm: false
+    }), react(),
+    ]
+  });
